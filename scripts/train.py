aefbd89ff22780aab7bd8d05129bd56d7639bd82
aryoerlangga <aryoerlangga11@yahoo.com>
2020-05-03 01:31:42 +0700

    add home path

--- scripts/train.py
+++ scripts/train.py
@@ -4,10 +4,14 @@ import pickle
 from utils_facenet import load_dataset, preprocessing
 from sklearn.preprocessing import LabelEncoder
 
-directory = 'images/train/'
+from pathlib import Path
+home = str(Path.home())
+path_global = home + '/face_recognition_facenet/'
+
+directory = path_global + 'images/train/'
 X_train, y_train = load_dataset(directory)
 
-model_facenet = load_model('model/facenet_keras.h5')
+model_facenet = load_model(path_global + 'model/facenet_keras.h5')
 
 X_train = preprocessing(X_train, model_facenet)
 
@@ -20,8 +24,8 @@ model.fit(X_train, y_train)
 
 
 # Save label_encoder and model to pickle
-encoder_filename = 'model/target_encoder.pkl'
+encoder_filename = path_global + 'model/target_encoder.pkl'
 pickle.dump(out_encoder, open(encoder_filename, 'wb'))
 
-model_filename = 'model/facereco.pkl'
+model_filename = path_global + 'model/facereco.pkl'
 pickle.dump(model, open(model_filename, 'wb'))
